045c160c79a37c95ee1c949b825ad119de50a2e5
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-05-06 18:53:31 +0200

    add: lcs dynamic programming solution

--- js/longest_common_subsequence.js
+++ js/longest_common_subsequence.js
@@ -1,9 +1,31 @@
 
 // longest common subsequence (LCS)
 
+// Dynamic programming approach
+// Time complexity: O(mn)
+const lcs = (
+  a,
+  b,
+  ia = a.length,
+  ib = b.length) => {
+  const len = new Array(ia + 1).fill(0).map(_ => []);
+  for (let i = 0; i <= ia; ++i) {
+    for (let j = 0; j <= ib; ++j) {
+      if (i === 0 || j === 0) {
+        len[i][j] = 0;
+      } else if (a[i-1] == b[j-1]) {
+        len[i][j] = len[i-1][j-1] + 1;
+      } else {
+        len[i][j] = Math.max(len[i-1][j], len[i][j-1]);
+      }
+    }
+  }
+  return len[ia][ib];
+};
+
 // worst case:
 // time complexity O(2^n)
-const lcs = (
+const lcs2 = (
   a,
   b,
   ia = a.length,
@@ -18,6 +40,5 @@ const lcs = (
     lcs(a, b, ia-1, ib));
 };
 
-
-
 console.log(lcs('ABCD', 'ABDC'));
+console.log(lcs2('ABCD', 'ABDC'));
