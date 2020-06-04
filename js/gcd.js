f5504bce38b07bfa70c3ea7e2d1056a53fcc3806
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-06-04 19:51:10 +0200

    gcd

---
+++ js/gcd.js
@@ -0,0 +1,20 @@
+
+// greatest common divisor
+
+// iterative solution:
+function gcd(a, b) {
+  if (a < 0) a = -a;
+  if (b < 0) b = -b;
+  if (b > a) {
+    [a, b] = [b, a];
+  }
+  while (1) {
+    if (b == 0) return a;
+    a %= b;
+    if (a == 0) return b;
+    b %= a;
+  }
+}
+
+// recursive solution:
+const gcd2 = (a, b) => b ? gcd2(b, a % b) : a;
