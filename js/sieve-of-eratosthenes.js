2ff2fc07906e95e619ba1eac801014e83084b05a
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-07-29 08:57:29 +0200

    add sieve-of-eratosthenes.js

---
+++ js/sieve-of-eratosthenes.js
@@ -0,0 +1,31 @@
+
+// moved from https://gist.github.com/gkucmierz/38323c91c2d2efe03c0dac43c106eda1
+
+const Sieve = (max = 1e5) => {
+  const maxi = Math.sqrt(max);
+  const notPrime = new Int8Array(max);
+  notPrime[0] = notPrime[1] = 1;
+  for (let i = 2; i < maxi; ++i) {
+    if (notPrime[i] === 0) {
+      for (let j = 2*i; j < max; j += i) {
+        notPrime[j] = 1;
+      }
+    }
+  }
+  return {
+    isPrime: n => !notPrime[n],
+    getPrimes: n => {
+      const res = [];
+      let cnt = 0;
+      for (let i = 0; i <= n; ++i) {
+        if (!notPrime[i]) {
+          res[cnt++] = i;
+        }
+      }
+      return res;
+    }
+  };
+};
+
+const sieve = Sieve(1e5);
+sieve.getPrimes(1e3).join(' ');
