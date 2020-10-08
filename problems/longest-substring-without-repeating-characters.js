4e3ea16d30d9e284917839be0956197a9c7f8b81
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-10-09 01:21:24 +0200

    add problem longest-substring-without-repeating-characters

---
+++ problems/longest-substring-without-repeating-characters.js
@@ -0,0 +1,43 @@
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters
+
+const lengthOfLongestSubstring = (s, result = _ => _) => {
+  if (s === '') return 0;
+  const set = new Set();
+  let lastIdx = 0;
+  let max = 0;
+  let [maxs, maxe] = [0, 0];
+  for (let i = 0; i < s.length; ++i) {
+    const c = s[i];
+    if (set.has(c)) {
+      for (lastIdx; lastIdx < i; ++lastIdx) {
+        const rc = s[lastIdx];
+        set.delete(rc);
+        if (rc === c) {
+          lastIdx++;
+          break;
+        }
+      }
+    }
+    set.add(c);
+    const dist = i - lastIdx;
+    if (dist > max) {
+      max = dist;
+      [maxs, maxe] = [lastIdx, i];
+    }
+  }
+  result(s.slice(maxs, maxe + 1));
+  return max + 1;
+};
+
+[
+  'abcabcbb',
+  'abcabcbb',
+  'bbbbb',
+  'pwwkew',
+].map(str => {
+  console.log(lengthOfLongestSubstring(str, res => {
+    console.log(str, res);
+  }));
+  console.log('-'.repeat(40))
+});
