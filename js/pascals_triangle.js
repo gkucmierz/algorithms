0605ad66a5ff461e13fa5c67f73fc78d9f0f88cf
Grzegorz Kucmierz <gkucmierz@gmail.com>
2022-02-02 18:46:39 +0100

    pascals triangle - add single row generation

--- js/pascals_triangle.js
+++ js/pascals_triangle.js
@@ -14,3 +14,18 @@ const pascalsTriangle = rows => {
   }
   return res;
 };
+
+
+const pascalsTriangleRow = row => {
+  let lvl = [1];
+  while (row--) {
+    const tmp = [...lvl, 0];
+    for (let i = tmp.length - 1; i > 0; --i) {
+      tmp[i] += tmp[i-1]
+    }
+    lvl = tmp;
+  }
+  return lvl;
+};
+
+console.log(pascalsTriangleRow(49)[6]);
