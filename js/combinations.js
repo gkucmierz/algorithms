76b72bb3e6151c01a8cc4f1f258a11986f6f7c16
Grzegorz Kućmierz <gkucmierz@users.noreply.github.com>
2024-03-26 16:32:57 +0100

    Update combinations.js

--- js/combinations.js
+++ js/combinations.js
@@ -4,7 +4,7 @@
 // combinations quantity formula:
 // k! / n! * (k-n)!
 
-const combinations = (n, k) => {
+const combinationsRecursive = (n, k) => {
   const arr = new Array(n).fill(0).map((_, i) => i);
   const res = [];
   
@@ -26,3 +26,53 @@ const combinations = (n, k) => {
 console.log(
   combinations(6, 10).length
 );
+
+// combinations without recursion
+const combinations = (n, k) => {
+  const res = [];
+  const max = k ** n;
+  for (let i = 0; i < max; ++i) {
+    let m = i;
+    const arr = [];
+    let last = k;
+    for (let j = n - 1; j >= 0; --j) {
+      const rem = m % k;
+      if (rem >= last) {
+        last = -1;
+        break;
+      }
+      arr[j] = rem;
+      m = (m - rem) / k;
+      last = rem;
+    }
+    if (last === -1) continue;
+    res.push(arr);
+  }
+  return res;
+};
+
+// combinations iterator
+const combinationsIterator = function* (n, k) {
+  const max = k ** n;
+  for (let i = 0; i < max; ++i) {
+    let m = i;
+    const arr = [];
+    let last = k;
+    for (let j = n - 1; j >= 0; --j) {
+      const rem = m % k;
+      if (rem >= last) {
+        last = -1;
+        break;
+      }
+      arr[j] = rem;
+      m = (m - rem) / k;
+      last = rem;
+    }
+    if (last === -1) continue;
+    yield arr;
+  }
+};
+
+for (const c of combinationsIterator(6, 49)) {
+  console.log(c.join(' '));
+};
